c726c0140bb2676732aa9770b05b10cc170a35d8
Agata Wajda <agawaj98@gmail.com>
2023-11-21 21:53:49 +0100

    init step_01

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,48 @@
+import { Component } from 'react';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <h1>Please leave your feedback</h1>
+
+      <MyClassComponent></MyClassComponent>
     </div>
   );
 };
+
+class MyClassComponent extends Component {
+  constructor() {
+    super();
+    this.state = {
+      good: 0,
+      bad: 0,
+      neutral: 0,
+    };
+  }
+
+  goodCounter = () => {
+    this.setState({ good: this.state.good + 1 });
+  };
+
+  neutralCounter = () => {
+    this.setState({ neutral: this.state.neutral + 1 });
+  };
+
+  badCounter = () => {
+    this.setState({ bad: this.state.bad + 1 });
+  };
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.goodCounter}>Good</button>
+        <button onClick={this.neutralCounter}>Neutral</button>
+        <button onClick={this.badCounter}>Bad</button>
+        <p>Good: {this.state.good}</p>
+        <p>Neutral: {this.state.neutral}</p>
+
+        <p>Bad: {this.state.bad}</p>
+      </div>
+    );
+  }
+}
